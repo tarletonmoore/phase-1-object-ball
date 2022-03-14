8bdda9b68935e51dcff6e59d3cdc4f46a27657a7
John Tarleton Moore <tarletonmoore@gmail.com>
2022-03-14 13:28:40 -0400

    commit tree 783fca0e09f9bdb976d1556ef79c3e88ba39d80d

---
+++ src/00-objectball.js
@@ -0,0 +1,137 @@
+function gameObject() {
+    return {
+        home: {
+            teamName: `Brooklyn Nets`, colors: [`Black, White`],
+            players: {
+                AlanAnderson: {
+                    number: 0,
+                    shoe: 16,
+                    points: 22,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 1
+                },
+                ReggieEvans: {
+                    number: 30,
+                    shoe: 14,
+                    points: 12,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    slamDunks: 7
+                },
+                BrookLopez: {
+                    number: 11,
+                    shoe: 17,
+                    points: 17,
+                    rebounds: 19,
+                    assists: 10,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 15
+                },
+                MasonPlumlee: {
+                    number: 1,
+                    shoe: 19,
+                    points: 26,
+                    rebounds: 12,
+                    assists: 6,
+                    steals: 3,
+                    blocks: 8,
+                    slamDunks: 5
+                },
+                JasonTerry: {
+                    number: 31,
+                    shoe: 15,
+                    points: 19,
+                    rebounds: 2,
+                    assists: 2,
+                    steals: 4,
+                    blocks: 11,
+                    slamDunks: 1
+                }
+            }
+        },
+        away: {
+            teamName: `Charlotte Hornets`,
+            colors: [`Torquoise, Purple`],
+            players: {
+                JeffAdrien: {
+                    number: 4,
+                    shoe: 18,
+                    points: 0,
+                    rebounds: 1,
+                    assists: 1,
+                    steals: 2,
+                    blocks: 7,
+                    slamDunks: 2
+                },
+                BismakBiyombo: {
+                    number: 0,
+                    shoe: 16,
+                    points: 12,
+                    rebounds: 4,
+                    assists: 7,
+                    steals: 7,
+                    blocks: 15,
+                    slamDunks: 10
+                }
+            },
+            DeSagnaDiop: {
+                number: 2,
+                shoe: 14,
+                points: 24,
+                rebounds: 12,
+                assists: 12,
+                steals: 4,
+                blocks: 5,
+                slamDunks: 5
+            },
+            BenGordon: {
+                number: 8,
+                shoe: 15,
+                points: 33,
+                rebounds: 3,
+                assists: 2,
+                steals: 1,
+                blocks: 1,
+                slamDunks: 0
+            },
+            BrendanHaywood: {
+                number: 33,
+                shoe: 15,
+                points: 6,
+                rebounds: 12,
+                assists: 12,
+                steals: 22,
+                blocks: 5,
+                slamDunks: 12
+            }
+        }
+    }
+}
+
+function homeTeam() {
+    return gameObject().home
+}
+
+function awayTeam() {
+    return gameObject().away
+}
+
+function players() {
+    return Object.assign({}, homeTeam.players, awayTeam.players)
+}
+
+
+function numPointsScored(playerInput) {
+    for (const playerName in players) {
+        if (playerName === playerInput) {
+            return players()[playerName].points
+        }
+    }
+
+}
